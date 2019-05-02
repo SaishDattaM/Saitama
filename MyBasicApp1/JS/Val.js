75f523e14f1c0d3914d7512a7f23dd1945e81e04
Musinada <MusinaSD@ES.AD.ADP.com>
2019-05-02 14:09:23 +0530

    Implemented validations

--- MyBasicApp1/JS/Val.js
+++ MyBasicApp1/JS/Val.js
@@ -4,21 +4,80 @@ $('#Login_form').submit(function(e){
     e.preventDefault();
    // window.alert('mmm');
     var username = $('#LoginUN').val();
-    var $usernameRegExp= /^[a-zA-Z]+$*/;
+    var usernameRegExp= /^[a-zA-Z]+$/;
     var password = $('#LoginPWD').val();
-    if(username.length==0 || password.length==0 )
-    {$('#msgval').text("username and password are mandatory");}
-    if (password.length < 8) {
-        $('#msgval').text("password min length is 8");
-        $('#LoginPWD').focus()
-        //window.alert('mmmdddddddd');
-    }else{$('#msgval').text("");}
+    var messagevalidation = $('#valmsg').val();
+    var valtest = usernameRegExp.test(username)
+    $('#valmsg').text("");
+    $('.error').remove();
+    var validation = true;
 // Validating Username Field.
-if (!(username.length >= 6 && username.length <= 8) && username.match($usernameRegExp) || username.length == 0) {
-$('#p2').text("* Please enter between 6 and 8 alphabets only *"); 
+if(username.length<1)
+{    // $('#valmsg').text("* Username and Password are mandatory *");
+$('#LoginUN').after('<span class="error">This field is required</span>');
+validation = false;
+//$("#LoginUN").focus();
+}else
+if (!(username.length >= 6 && username.length <= 8 && valtest==true)) {
+$('#LoginUN').after('<span class="error">Please enter between 6 and 8 alphabets only</span>');
 $("#LoginUN").focus();
-return false;
+validation = false;
 }
-    //window.location.href='User.html'
-    })
+//validating password field.
+if(password.length<1)
+{       $('#LoginPWD').after('<span class="error">This field is required</span>');
+validation = false;
+} else
+if (password.length < 8) {
+    $('#LoginPWD').after('<span class="error">password min length is 8</span>');
+    $('#LoginPWD').focus()
+    validation = false;
+}
+if(validation == true)
+{
+window.location.href='User.html'
+   }
+   });
+$('#userForm').validate({
+    rules : {
+        StudentName: 'required',
+        StudentID: {
+            required:true,
+            minlength:5
+        },
+        gender : 'required',
+        Standard:'required',
+        emailid:'required',
+        phno:   'required'
+
+    },
+    messages:{
+        StudentName:'This field is required',
+        StudentID:'This field is required and min length is 5',
+        gender:'This field is required',
+        Standard:'This field is required',
+        emailid:'This field is required',
+        phno:'This field is required'
+
+    },
+    submitHandler: function(form) {
+        form.submit();  
+      }
+});
+$('#sigupform').validate({
+    rules:{
+        name:'required',
+        DOB:'required',
+        pwd:'required',
+        pwd2:{
+            equalto: '#pwd'
+        }
+    },
+    messages:{
+        name:'This field is required',
+        DOB:'DOB is required field',
+        pwd:'This is required field',
+        pwd2:'This is required field'
+    }
+})
 })
